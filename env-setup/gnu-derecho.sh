9dd60f8981927730111e68213ac99ac13cf1cf76
Rong Kong <rkong@derecho4.hsn.de.hpc.ucar.edu>
2026-02-17 11:10:57 -0700

    Update enviroment file and add ppro lib to CMakeLists.txt

--- env-setup/gnu-derecho.sh
+++ env-setup/gnu-derecho.sh
@@ -16,13 +16,14 @@ module use /glade/work/epicufsrt/contrib/spack-stack/derecho/modulefiles
 module load ecflow/5.8.4
 module load mysql/8.0.33
 
-#module use /glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.8.0/envs/ue-gcc-12.2.0/install/modulefiles/Core
-module use /glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.9.3/envs/ue-gcc-12.4/install/modulefiles/Core
-module load stack-gcc/12.4.0
-module load stack-cray-mpich/8.1.29
+module use /glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.8.0/envs/ue-gcc-12.2.0/install/modulefiles/Core
+module load stack-gcc/12.2.0
+module load stack-cray-mpich/8.1.25
 module load stack-python/3.11.7
 module load jedi-mpas-env
-#module unload bufr-query # bufr_query causes ioda build to fail
+module unload bufr-query # bufr_query causes ioda build to fail
+module use /glade/work/rkong/install/modulefiles  # K.R.
+module load bufr-query-0.0.4       # K.R. replace the loaded bufr-query-0.0.2, but not destroy dependency
 
 module list
 
